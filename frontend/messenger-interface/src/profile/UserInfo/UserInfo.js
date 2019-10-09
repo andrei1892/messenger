1b3cb61de2736cbcc34a8ba7d50b88d81b23e13c
Andrei Piti <andreipiti17@gmail.com>
2019-10-09 19:13:08 +0300

    Created an Icons wrapper component

--- frontend/messenger-interface/src/profile/UserInfo/UserInfo.js
+++ frontend/messenger-interface/src/profile/UserInfo/UserInfo.js
@@ -1,12 +1,18 @@
 import React from "react";
+import {ICONS} from '../../reusables/Icons/Icons';
 import "./UserInfo.css";
 
 const UserInfo = props => {
   return (
-    <header className="user-info d-flex flex-row">
-      <img className="mx-2" src="" alt="profile" />
-      <p className="mx-2">{props.data.firstname}</p>
-      <p className="mx-2">{props.data.lastname}</p>
+    <header className="d-flex flex-row">
+      <div className='info'>
+      {props.data.img ? <img className="mx-2" src="" alt="profile" /> : <ICONS type={'FaIcons'} name={'FaUserAlt'} iconClass={'mx-2'} iconWrapper={'profile-picture'}  />}
+      <span className="mx-2"> {props.data.firstname} {props.data.lastname}</span>
+      </div>
+      <div className='user-options'>
+        <ICONS type={'IoIcons'} name={'IoIosSettings'} title={'Settings'} iconClass = {'mx-1'} />
+        <ICONS type={'IoIcons'} name={'IoIosExit'} title={'LogOut'} iconClass = {'mx-1'} />
+      </div>
     </header>
   );
 };
---
+++ frontend/messenger-interface/src/reusables/Icons/Icons.js
@@ -0,0 +1,40 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import * as IoIcons from 'react-icons/io';
+import * as FaIncons from 'react-icons/fa';
+import * as MdIcons from "react-icons/md";
+import * as TiIcons from "react-icons/ti";
+import * as GoIcons from "react-icons/go";
+import * as GiIcons from "react-icons/gi";
+import * as WiIcons from "react-icons/wi";
+
+const Icon_Type = {
+    IoIcons : IoIcons,
+    FaIcons: FaIncons,
+    MdIcons: MdIcons,
+    TiIcons: TiIcons,
+    GoIcons: GoIcons,
+    GiIcons: GiIcons,
+    WiIcons: WiIcons
+}
+
+ const ICONS = (props) => {
+    const {type, name, iconClass, iconWrapper, title} = props;
+    const Icon = Icon_Type[type][name];
+    const iconTitle = title ? title : null;
+    return(
+        <div className={iconWrapper} > 
+        <Icon className={iconClass} title = {iconTitle}/>
+        </div>
+    )
+}
+
+ICONS.propTypes = {
+    type: PropTypes.string,
+    name: PropTypes.string,
+    iconClass: PropTypes.string,
+    iconWrapper: PropTypes.string,
+    title: PropTypes.string
+}
+
+export {ICONS};
