5807e4bc778d30a99d13dd3e4ea7cbeac80b5512
Andrei Piti <andreipiti17@gmail.com>
2019-10-31 17:54:25 +0200

    Designing settings menu v1.0

--- frontend/messenger-interface/src/profile/ProfileSettings/ProfileSettings.js
+++ frontend/messenger-interface/src/profile/ProfileSettings/ProfileSettings.js
@@ -1,15 +1,44 @@
 import React, {Component} from 'react';
 import './ProfileSettings.css';
 
+import {configSettingsMenu} from './SettingsOptions';
+
+// const configMenu = {
+//     personal: 'personalInfo',
+//     language: 'language',
+//     security: 'security'
+// };
 class ProfileSettings extends Component {
-    render() {
+
+constructor(props) {
+    super();
+    this.state = {
+            currentMenu: ''
+        }
+    }
+  
+  renderList = () => {
+    return ( 
+            <ul className='settings-list' >
+                    <li className='settings-list-item'> Personal Info </li>
+                    <li className='settings-list-item'> Language </li>
+                    <li className='settings-list-item'> Security </li>
+            </ul>
+    );
+  }
+
+  setMenu = (type) => {
+    this.setState({currentMenu: type})
+  }
+
+  render() {
          return ( 
         <div className='profile-settings-menu'>
             <div className='sidebar-wrapper'>
-                <ul className='settings-list' >
-                    <li className='settings-list-item'> Personal Info </li>
-                    <li className='settings-list-item'> Language </li>
-                </ul>
+              {this.renderList()}
+            </div>
+            <div className='form-menu-container'>
+
             </div>
         </div>
         )
